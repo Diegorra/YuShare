cfe14c11ecbe55a92b270ffc29c4ad0565ec9454
Carlos Martín <79253407+eskei-ucm@users.noreply.github.com>
2022-04-13 22:11:57 +0200

    Update perfil.php

--- perfil.php
+++ perfil.php
@@ -1,29 +1,37 @@
 <?php
 
 require_once __DIR__.'/includes/config.php';
+use es\ucm\fdi\aw\Pelicula;
 
-$tituloPagina = 'Portada';
+$peliculasUsuario = Pelicula::peliculasPerfil($app->idUsuario());
+$tituloPagina = 'Perfil';
 $contenidoPrincipal=<<<EOS
-  <h1>John Doe</h1>
+  <h1>.</h1>
     <div class="card">
-        <img src="images/cara1.jpg" alt="Steven Spielberg" style="width:100%">
-        <h1>Steven Spielberg</h1>
+        <img src="images/cara1.jpg" alt="{$app->nombreUsuario()}" style="width:50%">
+        <h1>{$app->nombreUsuario()}</h1>
         <p class="title">Director</p>
     </div>
-
     <div class="texto_inicio">
-  		<img src="images/peliculas/avengers.jpg" id="image_inicio" title="Avengers">
-        <img src="images/peliculas/vida.jpg" id="image_inicio" title="La vida es bella">
-        <img src="images/peliculas/wayDown.jpg" id="image_inicio" title="Way Down">
-        <img src="images/peliculas/star_wars.jpg" id="image_inicio" title="Star Wars">
+      $peliculasUsuario
     </div>
+  
+  
+  
 
-    <div class="editarPerfil">
-        <a href="editarPerfil.php">Editar perfil</a>
-    </div>
   EOS;
+
 
 $params = ['tituloPagina' => $tituloPagina, 'contenidoPrincipal' => $contenidoPrincipal];
 $app->generaVista('/plantillas/plantilla.php', $params);
 
+/*
 
+
+    <div class="editarPerfil">
+        <a href="editarPerfil.php">Editar perfil</a>
+    </div>
+
+
+*/
+?>
