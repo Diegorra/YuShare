1cffa6cace6fb02a07f197c2ec3b476e70490a55
Carlos Martín <79253407+eskei-ucm@users.noreply.github.com>
2022-04-14 13:53:23 +0200

    Update perfil.php

--- perfil.php
+++ perfil.php
@@ -2,17 +2,27 @@
 
 require_once __DIR__.'/includes/config.php';
 use es\ucm\fdi\aw\Pelicula;
-
+use es\ucm\fdi\aw\usuarios\Usuario;
+$usuario = Usuario::buscaUsuario($app->nombreUsuario());
 $peliculasUsuario = Pelicula::peliculasPerfil($app->idUsuario());
 $tituloPagina = 'Perfil';
 $contenidoPrincipal=<<<EOS
   <h1>.</h1>
-    <div class="card">
-        <img src="images/cara1.jpg" alt="{$app->nombreUsuario()}" style="width:50%">
-        <h1>{$app->nombreUsuario()}</h1>
-        <p class="title">Director</p>
-    </div>
-    <div class="texto_inicio">
+  <h1>.</h1>
+    <div class='card'>
+        <img src='{$usuario->getImage()}' id="image_perfil">
+        <br>
+        <a href= "editarPerfil.php"> Editar perfil</a>
+        </div>
+        <div class='cardRightText'>
+          <h1>{$usuario->getNombreUsuario()}</h1>
+          <h1>{$usuario->getEmail()}</h1>
+          <p class="title">Director</p>
+        </div>
+        
+    
+    <div class="texto_perfil">
+    <p> Tus películas: </p>
       $peliculasUsuario
     </div>
   
@@ -25,7 +35,7 @@ $contenidoPrincipal=<<<EOS
 $params = ['tituloPagina' => $tituloPagina, 'contenidoPrincipal' => $contenidoPrincipal];
 $app->generaVista('/plantillas/plantilla.php', $params);
 
-/*
+/*<div class="card">
 
 
     <div class="editarPerfil">
