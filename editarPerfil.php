48fb1a6f59fb0d478bb5f62adb1540da6065debd
Diego <56647811+Diegorra@users.noreply.github.com>
2022-04-15 17:06:17 +0200

    Barra de búsqueda solo en index

--- editarPerfil.php
+++ editarPerfil.php
@@ -5,7 +5,7 @@ require_once __DIR__.'/includes/config.php';
 $formSettings = new \es\ucm\fdi\aw\usuarios\FormularioSettings();
 $formSettings = $formSettings->gestiona();
 
-$tituloPagina = 'Portada';
+$tituloPagina = 'Settings';
 $contenidoPrincipal=<<<EOS
     <div class ="editar">
 		<h1>Edita tu perfil</h1>
@@ -18,16 +18,6 @@ $app->generaVista('/plantillas/plantilla.php', $params);
 
 
 
-/*
-<fieldset>
-			<legend>Datos del perfil</legend>
-				Nombre:<br> <input type="text" name="nom"><br>
-				Email:<br> <input type="text" name="email"><br>
-				Nombre de usuario:<br> <input type="text" name="user"><br>
-				Teléfono: <br> <input type="text" name="phone"><br>
-		</fieldset>
-	<input type="submit" name="aceptar">
-*/
 
 
 
